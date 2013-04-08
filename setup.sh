c9988dcc069d99ba63b0a828b737f11bfb489b0d
Sanjiv Raj <sanjiv.raj@gmail.com>
2013-04-08 12:46:22 +0530

    Refactor

--- setup.sh
+++ setup.sh
@@ -1,13 +1,26 @@
-ln -sf `pwd`/vimrc  ~/.vimrc
-ln -sf `pwd`/inputrc ~/.inputrc
-ln -sf `pwd`/bash_profile ~/.bash_profile
-ln -sf `pwd`/bashrc ~/.bashrc
-ln -sf `pwd`/gitconfig ~/.gitconfig
-ln -sf `pwd`/screenrc ~/.screenrc
-ln -sf `pwd`/tmux.conf ~/.tmux.conf
+mkdir -p ~/.shell_startup
+ln -sf `pwd`/shell/aliases 	~/.shell_startup/aliases
+ln -sf `pwd`/shell/environment 	~/.shell_startup/environment
+ln -sf `pwd`/shell/eyecandy	~/.shell_startup/eyecandy
+ln -sf `pwd`/shell/inputrc 	~/.inputrc
 
-# ln -sf `pwd`/xsession ~/.xsession
+ln -sf `pwd`/shell/bashrc 	~/.bashrc
+ln -sf `pwd`/shell/profile 	~/.profile
 
-ln -sf `pwd`/dhexception.vim ~/.vim/
+
+ln -sf `pwd`/gitconfig 		~/.gitconfig
+ln -sf `pwd`/screenrc 		~/.screenrc
+ln -sf `pwd`/tmux.conf 		~/.tmux.conf
+
+ln -sf `pwd`/ssh_config		~/.ssh/config
+
+# ln -sf `pwd`/xsession 	~/.xsession
+
+
+
+ln -sf `pwd`/vimrc  		~/.vimrc
+ln -sf `pwd`/dhexception.vim 	~/.vim/
 mkdir -p ~/.vim/syntax/
-ln -sf `pwd`/pig.vim ~/.vim/syntax/
+ln -sf `pwd`/pig.vim 		~/.vim/syntax/
+
+ln -sf `pwd`/bin		~/bin
