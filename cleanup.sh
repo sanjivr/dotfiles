556a3371cfd47a678d4248f9e01d0d4ea8f96607
Sanjiv Raj <sanjiv.raj@gmail.com>
2013-04-08 12:46:22 +0530

    Refactor

--- cleanup.sh
+++ cleanup.sh
@@ -1,13 +1,24 @@
-rm ~/.vimrc
-rm ~/.inputrc
-rm ~/.bash_profile
+rm -rf ~/.shell_startup
+
 rm ~/.bashrc
+rm ~/.profile
+
+rm ~/.inputrc
+
+rm ~/.vimrc
 rm ~/.gitconfig
 rm ~/.screenrc
 rm ~/.tmux.conf
+rm ~/.ssh/config
+
 
 # rm ~/.xsession
 
 rm ~/.vim/dhexception.vim
 rm ~/.vim/syntax/pig.vim
 rm -rf ~/.vim/syntax
+
+# Cleanup - No need for these scripts ever
+rm ~/.bash_aliases
+rm ~/.bash_profile
+rm ~/.bash_login
